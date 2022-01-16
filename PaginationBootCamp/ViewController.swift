2e9a84f0d35c9989c8ac155d6edd6dc97693178d
Rijo <rizosamuel1995@gmail.com>
2022-01-16 16:32:57 +0530

    Initial Commit

---
+++ PaginationBootCamp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PaginationBootCamp
+//
+//  Created by Rijo Samuel on 16/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
